624936631b44f2d0ceb5a1ce262bd0b36ebdef7b
andrhwl <120339957+andrhwl@users.noreply.github.com>
2023-10-18 23:32:11 +0300

    add

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,6 +2,19 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
+
+type page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+
 const page1 = {
   title: 'The awesome page',
   likes: 100,
