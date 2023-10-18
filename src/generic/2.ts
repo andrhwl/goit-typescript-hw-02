187f1d5884e68b94668ad435809098fabd73a3d5
andrhwl <120339957+andrhwl@users.noreply.github.com>
2023-10-18 23:46:19 +0300

    фвв

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -9,15 +9,17 @@ type AllType = {
   position: number;
   color: string;
   weight: number
-}
+};
+type obj1 = Pick<AllType, "name" | "position" | "color" | "weight">;
+type obj2 = Pick<AllType, "name" | "position" | "color" | "weight">;
 
-function compare (top, bottom): AllType {
+function compare (top: obj1, bottom: obj2): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface ComponentProp {
+  title: string;
+}
+
+class Component<T extends ComponentProp> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<ComponentProp> {
   pageInfo () {
     console.log(this.props.title);
   }
